6f826749a3120a19140dff92ec98769c6619e451
sixth-of-august <happycheshircat@gmail.com>
2025-10-07 01:59:07 +0900

    полнаяя готовая версия программы

--- progression.cpp
+++ progression.cpp
@@ -1,6 +1,9 @@
-#include <iostream>
+#include <iostream> // модуль для основных функций программы
+#include <string> // модуль для расширенных ошибок
 
-#include "progression.h"
+#include "progression.h" // заголовочный файл программы
+
+using namespace std;
 
 /* @brief функция для вычисления суммы прогрессии n-членов
    @param first Первый член прогрессии
@@ -9,7 +12,13 @@
    @return Сумма n-членов прогрессии*/
 double getProgression(double first, double diff, int amount){
 
+// проверка на положительное количество элементов для суммы
+if(amount <= 0){
+   // вывод сообщения об ошибке через throw
+   throw string("Error: Количество элементов не может быть <= 0");
+}
+
 // считаем последний член а.прогрессии
-double last = first +(amount - 1) * diff;
-return amount / 2 * (first + last); // считаем сумму членов
+double last = first + ((amount - 1) * diff);
+return (amount / 2.0) * (first + last); // считаем сумму членов
 }
